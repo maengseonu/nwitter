cbae1a62bad43ad62ad093a88bea920c07c0aaa2
maengseonu <aodtjsdn123@gmail.com>
2021-11-29 16:53:25 +0900

    계정 생성, 로그인, 소셜 로그인

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import { authService } from "../fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -13,8 +23,32 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
-    event.preventDegault();
+  const onSubmit = async (event) => {
+    const auth = getAuth();
+    event.preventDefault();
+    try {
+      if (newAccount) {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        await signInWithEmailAndPassword(auth, email, password);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const auth = getAuth();
+    await signInWithPopup(auth, provider);
   };
   return (
     <div>
@@ -35,11 +69,17 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "계정 생성" : "로그인"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "로그인" : "계정 생성"}</span>
       <div>
-        <button>구글 로그인하기</button>
-        <button>깃허브 로그인하기</button>
+        <button onClick={onSocialClick} name="google">
+          구글 로그인하기
+        </button>
+        <button onClick={onSocialClick} name="github">
+          깃허브 로그인하기
+        </button>
       </div>
     </div>
   );
